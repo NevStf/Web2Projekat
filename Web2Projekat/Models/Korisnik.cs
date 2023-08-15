175ca26ed6cb3aec0a7dd20f5626282c1187cbfd
NevStf <Nevena_98_Cuprija@hotmail.com>
2023-08-15 20:52:12 +0200

    Klasa + konekcija sa bazom

---
+++ Web2Projekat/Models/Korisnik.cs
@@ -0,0 +1,22 @@
+﻿namespace Web2Projekat.Models
+{
+    public enum TIP_KORISNIKA { ADMINISTRATOR, PRODAVAC, KUPAC }
+    public class Korisnik
+    {
+        public string KIme { get; set; }
+        public string Lozinka { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Adresa { get; set; }
+        public string EmailAdresa { get; set; }
+        public DateTime DatumRodjenja { get; set; }
+
+        //overriduj datum
+        public string DRodjenja { get; set; }
+
+        public TIP_KORISNIKA Tip { get; set; }
+      
+        //dodaj upload slike
+
+    }
+}
