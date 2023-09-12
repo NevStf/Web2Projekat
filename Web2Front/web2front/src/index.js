8be1949bbe19739368ab210e9fad343968343ec5
NevStf <Nevena_98_Cuprija@hotmail.com>
2023-09-12 22:42:41 +0200

    mrzim front pt3?

--- Web2Front/web2front/src/index.js
+++ Web2Front/web2front/src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Register from './components/register/register';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
