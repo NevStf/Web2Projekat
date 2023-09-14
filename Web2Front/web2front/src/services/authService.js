1b5afbcc7073da5e22df04d59bd946ab7beb949a
NevStf <Nevena_98_Cuprija@hotmail.com>
2023-09-14 22:18:58 +0200

    :(

--- Web2Front/web2front/src/services/authService.js
+++ Web2Front/web2front/src/services/authService.js
@@ -12,11 +12,7 @@ export const userLogin = async (korisnickoIme, lozinka) => {
   };
   
   export const userRegister = async (formData) => {
-    console.log(process.env);
-    console.log(formData)
-    console.log("IDE OVDE JEBIGA");
     console.log(process.env.REACT_APP_REGISTER_ENDPOINT);
-    console.log("IDE OVDE JEBIGA");
     return fetch(`${process.env.REACT_APP_REGISTER_ENDPOINT}`, {
       method: "POST",
       headers: {
